d381f688ad5653571fda310397d8bbc7596b1618
大石头 <nnhy@vip.qq.com>
2022-05-03 18:15:12 +0800

    驱动参数增加备注，生成参数模版Xml时将会带上注释

--- NewLife.Omron/Drivers/OmronParameter.cs
+++ NewLife.Omron/Drivers/OmronParameter.cs
@@ -1,16 +1,20 @@
-﻿using NewLife.IoT.Drivers;
+﻿using System.ComponentModel;
+using NewLife.IoT.Drivers;
 
 namespace NewLife.Omron.Drivers;
 
 /// <summary>Omron参数</summary>
 public class OmronParameter : IDriverParameter
 {
-    /// <summary>地址</summary>
+    /// <summary>地址。例如 127.0.0.1:9600</summary>
+    [Description("地址。例如 127.0.0.1:9600")]
     public String Address { get; set; }
 
-    /// <summary>PLC的单元号地址，通常都为0</summary>
+    /// <summary>PLC的单元号地址。默认0</summary>
+    [Description("PLC的单元号地址。默认0")]
     public Byte DA2 { get; set; }
 
     /// <summary>数据格式。ABCD/BADC/CDAB/DCBA</summary>
+    [Description("数据格式。ABCD/BADC/CDAB/DCBA")]
     public String DataFormat { get; set; }
 }
